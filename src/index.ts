35820491e8e4c725100d31fe1b7c94cd08630043
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-08-14 21:17:33 +0200

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,15 @@ import * as path from 'path';
 //env config
 const envFile = path.resolve(__dirname, 'connection.env');
 dotenv.config({ path: envFile });
-const connection_string = process.env.CONNECTION_STRING_LOCAL;
+//database config -> local for db local, atlas for db atlas
+let database = 'local'
+let connection_string;
+if (database === 'local') connection_string = `${process.env.CONNECTION_STRING_LOCAL}/todo-list`
+else if (database === 'atlas') connection_string = process.env.CONNECTION_STRING;
 
 mongoose.set("debug", true);
 mongoose
-  .connect(`${connection_string}/todo-list`)
+  .connect(connection_string)
   .then((_) => {
     const port = process.env.PORT;
     app.listen(port, () => {
